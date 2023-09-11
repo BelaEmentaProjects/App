2297eba147898405035fb4fe91d26f0654d6f444
jndgoncalves <joao.n.d.goncalves@gmail.com>
2023-09-11 17:09:19 +0100

    Updated data path in dispatch to correctly map restaurant data

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -12,7 +12,8 @@ export default function Body() {
       try {
         axios.get('http://localhost:3002/restaurants').then((response) => {
           // Dispatch the transformed data to Redux Store
-          dispatch(updateRestaurants(response.data.results.data));
+          // Non mocked API (dispatch(updateRestaurants(response.data.results.data));
+          dispatch(updateRestaurants(response.data.data));
           console.log(
             '🚀 ~ file: Body.jsx:16 ~ axios.get ~ response.data:',
             response.data,
