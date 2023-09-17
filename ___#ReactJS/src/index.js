11f00871d8ee62c63609d862bfd3022baf5c76e6
jndgoncalves <joao.n.d.goncalves@gmail.com>
2023-09-17 21:38:53 +0100

    Integrated React Native project

--- ___#ReactJS/src/index.js
+++ ___#ReactJS/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './resources/css/style.css';
 import { BrowserRouter } from 'react-router-dom';
 
